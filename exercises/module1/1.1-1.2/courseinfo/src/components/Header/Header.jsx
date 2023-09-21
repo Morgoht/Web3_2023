9f498a4cbe80590ddf1a0a0dbf3b694912c484ec
Morgoht <amauryandrade1998@gmail.com>
2023-09-21 17:51:25 +0200

    1.1-1.2 : import image

--- exercises/module1/1.1-1.2/courseinfo/src/components/Header/Header.jsx
+++ exercises/module1/1.1-1.2/courseinfo/src/components/Header/Header.jsx
@@ -1,6 +1,9 @@
+import imgLogo from './logo.png'
+
 const Header = ({course}) => {
     return (
         <div>
+            <img src={imgLogo}></img>
             <h1>{course}</h1>
         </div>
     )
