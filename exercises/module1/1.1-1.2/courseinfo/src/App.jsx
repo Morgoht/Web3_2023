a88ddd04abc9d1cf4a71a07b36950d63f4f9de0d
Morgoht <amauryandrade1998@gmail.com>
2023-09-21 16:32:29 +0200

    1.1-1.2 : basic components

---
+++ exercises/module1/1.1-1.2/courseinfo/src/App.jsx
@@ -0,0 +1,27 @@
+import Header from "./Header"
+import Content from "./Content"
+import Total from "./Total"
+
+
+
+function App() {
+  
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+  const exercises = [exercises1,exercises2,exercises3]
+  const parts = [part1, part2, part3]
+  return (
+    <>
+      <Header course = {course}/>
+      <Content parts = {parts}/>
+      <Total exercises = {exercises}/>
+    </>
+  )
+}
+
+export default App
---
+++ exercises/module1/1.1-1.2/courseinfo/src/Part.jsx
@@ -0,0 +1,8 @@
+const Part = ({part}) => {
+    return (
+        <div>
+            <p>{part}</p>
+        </div>
+    )
+}
+export default Part
