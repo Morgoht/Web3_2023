386cabacc8872b5ac1cc42a0e732a4279f592881
Morgoht <amauryandrade1998@gmail.com>
2023-10-06 13:21:07 +0200

    module 3 : 2.6-2.9

--- exercises/module3/src/components/Button/Button.jsx
+++
@@ -1,8 +0,0 @@
-const Button = ({label, handler}) => {
-    return (
-        <div>
-            <button className={label} onClick={handler}>{label}</button>
-        </div>
-    )
-}
-export default Button
